064e660f2374a59c110b6e0d942842cfff98e771
mischief <mischief@offblast.org>
2013-11-25 18:33:37 -0800

    fix modulo, thanks dcfn1

--- rotn.go
+++ rotn.go
@@ -2,12 +2,12 @@ package rottor
 
 func RotNEnc(n int) Filter {
 	return func(b byte) byte {
-		return byte((int(b) + n) % 255)
+		return byte((int(b) + n) % 256)
 	}
 }
 
 func RotNDec(n int) Filter {
 	return func(b byte) byte {
-		return byte((int(b) - n) % 255)
+		return byte((int(b) - n) % 256)
 	}
 }
